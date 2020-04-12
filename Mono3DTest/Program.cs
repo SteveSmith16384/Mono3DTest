5b9d5e4c4bdc776fab4ba0f447b7ad2b5150cf1e
Stephen Carlyle-Smith <stephen.carlylesmith@googlemail.com>
2020-04-12 16:06:00 +0100

    Initial checkin

---
+++ Mono3DTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Mono3DTest
+{
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Test3D.Test3DDemo())
+                game.Run();
+        }
+    }
+}
