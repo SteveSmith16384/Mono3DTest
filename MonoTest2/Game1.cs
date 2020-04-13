4ffc54f9de007a94bf2e3b4db218ee94a04db8d5
Stephen Carlyle-Smith <stephen.carlylesmith@googlemail.com>
2020-04-13 12:19:27 +0100

    Draw 2 sprites

--- MonoTest2/Game1.cs
+++ MonoTest2/Game1.cs
@@ -18,6 +18,8 @@ namespace MonoGame3D
 
         Texture2D checkerboardTexture;
 
+        VertexPositionTexture[] newSprite;
+
         //Vector3 cameraPosition = new Vector3(15, 10, 10);
         Vector3 cameraPosition = new Vector3(0, 0, 100);
 
@@ -31,6 +33,8 @@ namespace MonoGame3D
 
         protected override void Initialize()
         {
+            newSprite = this.CreateSprite(10, 10);
+
             floorVerts = new VertexPositionTexture[6];
 
             floorVerts[0].Position = new Vector3(-20, -20, 0);
@@ -73,6 +77,32 @@ namespace MonoGame3D
             base.Initialize();
         }
 
+
+        private VertexPositionTexture[] CreateSprite(float x, float y)
+        {
+            VertexPositionTexture[] verts = new VertexPositionTexture[6];
+
+            verts[0].Position = new Vector3(x-20, y-20, 0);
+            verts[1].Position = new Vector3(x-20, y+20, 0);
+            verts[2].Position = new Vector3(x+20, y-20, 0);
+
+            verts[3].Position = verts[1].Position;
+            verts[4].Position = new Vector3(x+20, y+20, 0);
+            verts[5].Position = verts[2].Position;
+
+            int repetitions = 1;
+
+            verts[0].TextureCoordinate = new Vector2(0, 0);
+            verts[1].TextureCoordinate = new Vector2(0, repetitions);
+            verts[2].TextureCoordinate = new Vector2(repetitions, 0);
+
+            verts[3].TextureCoordinate = verts[1].TextureCoordinate;
+            verts[4].TextureCoordinate = new Vector2(repetitions, repetitions);
+            verts[5].TextureCoordinate = verts[2].TextureCoordinate;
+
+            return verts;
+        }
+
         protected override void LoadContent()
         {
             checkerboardTexture = Content.Load<Texture2D>("ericbomb");
@@ -86,9 +116,10 @@ namespace MonoGame3D
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.SamplerStates[0] = SamplerState.PointClamp;
             GraphicsDevice.BlendState = BlendState.AlphaBlend; // Need for transparent bits
-            GraphicsDevice.DepthStencilState = DepthStencilState.Default;
+            GraphicsDevice.SamplerStates[0] = SamplerState.PointClamp;
+            //GraphicsDevice.SamplerStates[1] = SamplerState.PointClamp;
+            //GraphicsDevice.DepthStencilState = DepthStencilState.Default;
 
             GraphicsDevice.Clear(Color.Red);
 
@@ -111,6 +142,17 @@ namespace MonoGame3D
                     PrimitiveType.TriangleList,
                     // The array of verts that we want to render
                     floorVerts,
+                    // The offset, which is 0 since we want to start 
+                    // at the beginning of the floorVerts array
+                    0,
+                    // The number of triangles to draw
+                    2);
+
+                graphics.GraphicsDevice.DrawUserPrimitives(
+                    // We’ll be rendering two triangles
+                    PrimitiveType.TriangleList,
+                    // The array of verts that we want to render
+                    this.newSprite,
                     // The offset, which is 0 since we want to start 
                     // at the beginning of the floorVerts array
                     0,
